db61a8518da7fafda319c9aab5f60099abf821e4
Noe0131 <KTC24A31G0017@edu.kyoto-tech.ac.jp>
2026-04-14 09:37:01 +0900

    ブロックチェーンプロジェクト初期化

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use sha2::{Digest, Sha256};
+use std::fmt;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::thread;
+use std::time::Duration;
+
+// Defin difficulty of the mining
+const DIFFICULTY: usize = 2;
+// Defin the structure of a block in the blockchain
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
